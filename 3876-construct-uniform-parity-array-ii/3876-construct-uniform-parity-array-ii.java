8e46c4683982d699338232554f8be855042daf54
Rishika Verma <rishikav371@gmail.com>
2026-09-03 23:36:31 +0530

    Time: 2 ms (100%), Space: 121.6 MB (74.64%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int min =nums1[0];
+        for(int num :nums1){
+            if(num<min){
+                min=num;
+            }
+        }
+        if(min%2!=0){
+            return true;
+        }
+        for(int num :nums1){
+            if(num%2!=0){
+                return false;
+            }
+            
+        }
+        return true;
+    }
+}
