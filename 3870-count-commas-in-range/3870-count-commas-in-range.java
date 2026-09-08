49a044333570536f89e087bc4803375814e80f65
Rishika Verma <rishikav371@gmail.com>
2026-09-08 10:26:29 +0530

    Time: 0 ms (100%), Space: 42.5 MB (57.08%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -3,12 +3,6 @@ class Solution {
         if(n<1000){
             return 0;
         }
-        int totalComma =0;
-        int t=1000;
-        while(n>=t){
-           totalComma += (n-1000+1);
-           t*=1000;
-        }
-        return totalComma;
+        return n-999;
     }
 }
